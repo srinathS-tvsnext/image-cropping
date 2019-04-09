52ed332384ca055dbdab104d47901fe50b0b548e
Srinath S <srinath.s@tvsnext.io>
2019-04-09 16:24:52 +0530

    pushintogit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomecomponentComponent } from './homecomponent/homecomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomecomponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule, MatCheckboxModule,MatFormFieldModule, MatInputModule, MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
